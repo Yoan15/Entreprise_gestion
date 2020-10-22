5ad88318699e3a25c777146a4608eeda62244e5c
Yoan <yoann.deco@gmail.com>
2020-10-22 16:21:02 +0200

    fusion formulaires + progression include

--- formulaire.php
+++ formulaire.php
@@ -13,53 +13,67 @@
         <h1>Formulaire employé</h1>
         
         <?php
+            $db = mysqli_init();
+            mysqli_real_connect($db, 'localhost', 'yoan', 'kongo','employer');
 
+            $action="";
+
+            if (isset($_GET["action"]) && $_GET["action"] == "add") {
+                $action="tableau_employe.php?action=add";
+            }
+
+            if (isset($_GET["action"]) && $_GET["action"] == "modif") {
+                $query= 'SELECT * FROM emp2 WHERE NOEMP=' .$_GET["NOEMP"];
+                $rs = mysqli_query($db, $query);
+                $data = mysqli_fetch_array($rs);
+                $action="tableau_employe.php?action=modif";
+            }
         ?>
 
         <div class="row">
-            <form action="tableau_employe.php?action=add" method="post">
+            <form action="<?php echo $action; ?>" method="post">
                 <div class="form col-12">
                     <label for="NOEMP">N°Employé</label>
-                    <input type="number" class="form-control" name="NOEMP" placeholder="0000" required>
+                    <input type="number" class="form-control" name="NOEMP" placeholder="0000" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[0]);} ?>" required>
 
                     <div class="form-group">
                         <label for="NOM">Nom</label>
-                        <input type="text" class="form-control" name="NOM" placeholder="Nom">
+                        <input type="text" class="form-control" name="NOM" placeholder="Nom" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[1]);} ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="PRENOM">Prenom</label>
-                        <input type="text" class="form-control" name="PRENOM" placeholder="Prenom">
+                        <input type="text" class="form-control" name="PRENOM" placeholder="Prenom" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[2]);} ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="EMPLOI">Poste</label>
-                        <input type="text" class="form-control" name="EMPLOI" placeholder="Poste">
+                        <input type="text" class="form-control" name="EMPLOI" placeholder="Poste" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[3]);} ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="SUP">Supérieur</label>
-                        <input type="number" class="form-control" name="SUP" placeholder="0000">
+                        <input type="number" class="form-control" name="SUP" placeholder="0000" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[4]);} ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="EMBAUCHE">Date embauche</label>
-                        <input type="date" class="form-control" name="EMBAUCHE">
+                        <input type="date" class="form-control" name="EMBAUCHE" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[5]);} ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="SAL">Salaire</label>
-                        <input type="number" class="form-control" name="SAL" placeholder="00000">
+                        <input type="number" class="form-control" name="SAL" placeholder="00000" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[6]);} ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="COMM">Commission</label>
-                        <input type="number" class="form-control" name="COMM" placeholder="0">
+                        <input type="number" class="form-control" name="COMM" placeholder="0" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[7]);} ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="NOSERV">N°Service</label>
-                        <input type="number" class="form-control" name="NOSERV" placeholder="0" required>
+                        <input type="number" class="form-control" name="NOSERV" placeholder="0" value="<?php if($action="tableau_employe.php?action=modif"){echo($data[8]);} ?>" required>
                     </div>
                 </div>
 
