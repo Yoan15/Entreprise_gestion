724bb3e1d931f6373272d9590b5a34f909bcbd6e
Yoan <yoann.deco@gmail.com>
2020-10-27 14:52:36 +0100

    progression

--- class/test.php
+++ class/test.php
@@ -5,19 +5,19 @@
 
     $employe1 = new Employe();
     $employe1->setNoemp(5557)->setNom("Stark")->setPrenom("Tony")->SetPoste("Iron Man")
-    ->SetSup(1000)->SetEmbauche("2020-10-27")->SetSalaire(50000)->SetComm(0)->SetServ(1);
+    ->SetSup(1000)->SetEmbauche("2020-10-27")->SetSalaire(50000)->SetServ(1);
 
     $employe2 = new Employe();
-    $employe2->setNoemp(5558)->setNom("Potts")->setPrenom("Peper")->SetPoste("Assistante")
+    $employe2->setNoemp(5558)->setNom("Pots")->setPrenom("Pepper")->SetPoste("Assistante")
     ->SetSup(5557)->SetEmbauche("2020-10-27")->SetSalaire(30000)->SetComm(5000)->SetServ(2);
 
     $employe3 = new Employe();
     $employe3->setNoemp(5559)->setNom("Xavier")->setPrenom("Charles")->SetPoste("Professeur")
-    ->SetSup(5557)->SetEmbauche("2020-10-27")->SetSalaire(30000)->SetComm(0)->SetServ(3);
+    ->SetSup(5557)->SetEmbauche("2020-10-27")->SetSalaire(30000)->SetServ(3);
 
     $employe4 = new Employe();
     $employe4->setNoemp(6000)->setNom("Grey")->setPrenom("Jean")->SetPoste("Scientifique")
-    ->SetSup(5557)->SetEmbauche("2020-10-27")->SetSalaire(30000)->SetComm(0)->SetServ(4);
+    ->SetSup(5557)->SetEmbauche("2020-10-27")->SetSalaire(30000)->SetServ(4);
 
     echo "$employe1 \n";
     echo "$employe2 \n";
