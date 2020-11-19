bcca5d7f2fd2f86acb491938c116ba6490fa0b49
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2020-11-19 12:14:10 +0100

    Création du tab pour exo tab JS

---
+++ exo_JavaScript/exo_tab.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="exo_tab.css">
+    <title>Exercice Tableau</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Graphic Card</th>
+                <th>Price</th>
+                <th>Brand</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>GTX 1660 Super</td>
+                <td>200$</td>
+                <td>PNY</td>
+            </tr>
+            <tr>
+                <td>RX 6800 XT</td>
+                <td>799$</td>
+                <td>Sapphire</td>
+            </tr>
+            <tr>
+                <td>RTX 3080</td>
+                <td>899$</td>
+                <td>Palit</td>
+            </tr>
+            <tr>
+                <td>RTX 2070 Super</td>
+                <td>539$</td>
+                <td>EVGA</td>
+            </tr>
+            <tr>
+                <td>RTX 2060</td>
+                <td>250$</td>
+                <td>MSI</td>
+            </tr>
+            <tr>
+                <td>RX 5700 XT</td>
+                <td>479$</td>
+                <td>ASRock</td>
+            </tr>
+        </tbody>
+    </table>
+    
+</body>
+</html>
